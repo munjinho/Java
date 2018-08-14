b3cad154f10f59dad7ef4e848ff68024a9e0acdd
Jinho <mjh9082@gmail.com>
2018-08-14 19:10:10 +0900

    Update Ex02.java

--- src/ch2/Ex02.java
+++ src/ch2/Ex02.java
@@ -1,15 +1,34 @@
-package ch2;
-class Ex02
-{
-	public static void main(String[] args)
-	{
-		double e1=7.125;
-		float e2=7.125F;
-		
-		long n1=10000000000L;
-		long n2=150;
-		
-		System.out.println(e1);
-		System.out.println(n1);	
+package ch1;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1.정수 -> 실수 
+		 * (4byte) -> (8byte)
+		 * 2.실수 -> 정수 
+		 * (8byte) -> (4byte)
+		 * 3.정수 -> 문자
+		 * (4byte) -> (1byte)
+		 */
+		//================================
+		//정수 상수 
+		int num = 65;
+		//실수 상수 
+		double dub = 97.5;
+		//문자 상수
+		char ch = 'a';//(힌트: 아스키: 97) 
+		//================================
+		//4byte -> 8byte
+		dub=(double)num;
+		System.out.println("double: "+dub);
+		//8byte -> 4byte 
+		num=(int)dub;
+		System.out.println("int: "+num);
+		//4byte -> 1byte
+		ch=(char)num;
+		System.out.println("char: "+ch);
+		//================================
+		System.out.println("ch == (char)num\n"+ (ch==(char)num));
 	}
 }
